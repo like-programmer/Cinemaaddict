06dd65ecf71c73adc79b0b492ea533888aab2e1a
Darya796 <dashutkadevi@gmail.com>
2020-05-01 14:45:41 +0300

    3.3.3 add render 5 more cards after click btn load more & remove btn if all of the cards showed

--- src/components/load-more-btn.js
+++ src/components/load-more-btn.js
@@ -1,4 +1,4 @@
-export const createLoadBtnTemplate = () => {
+export const createLoadMoreBtnTemplate = () => {
   return (`
     <button class="films-list__show-more">Show more</button>
     `);
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import {createSiteMenuTemplate} from "./components/site-menu.js";
 import {createSortingTemplate} from "./components/sorting.js";
 import {createBoardTemplate} from "./components/board.js";
 import {createFilmCardTemplate} from "./components/film-card.js";
-import {createLoadBtnTemplate} from "./components/load-btn.js";
+import {createLoadMoreBtnTemplate} from "./components/load-more-btn.js";
 import {createMoviesCountTemplate} from "./components/movies-count.js";
 import {createDetailsPopupTemplate} from "./components/details-popup.js";
 
@@ -18,7 +18,7 @@ const SHOWING_CARD_COUNT_BY_BUTTON = 5;
 const SHOWING_EXTRA_CARD_COUNT = 2;
 
 const filters = generateFilters();
-const filmCards = generateFilmCards(SHOWING_CARD_COUNT_ON_START);
+const filmCards = generateFilmCards(CARD_COUNT);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -45,7 +45,22 @@ for (let i = 0; i < showingCardCount; i++) {
   render(cardsListElement[0], createFilmCardTemplate(filmCards[i]), `beforeend`);
 }
 
-render(cardsListElement[0], createLoadBtnTemplate(), `afterend`);
+render(cardsListElement[0], createLoadMoreBtnTemplate(), `afterend`);
+
+const loadMoreBtn = siteMainElement.querySelector(`.films-list__show-more`);
+
+loadMoreBtn.addEventListener(`click`, () => {
+  const prevCardCount = showingCardCount;
+  showingCardCount = showingCardCount + SHOWING_CARD_COUNT_BY_BUTTON;
+
+  filmCards.slice(prevCardCount, showingCardCount).forEach((card) => {
+    render(cardsListElement[0], createFilmCardTemplate(card), `beforeend`);
+  });
+
+  if (showingCardCount >= filmCards.length) {
+    loadMoreBtn.remove();
+  }
+});
 
 for (let i = 0; i < SHOWING_EXTRA_CARD_COUNT; i++) {
   render(cardsListElement[1], createFilmCardTemplate(filmCards[i]), `beforeend`);

5e7077a13ca41130fd26521bc4115ae40c04f355
like-programmer <dashutkadevi@gmail.com>
2020-05-26 20:40:06 +0300

    6.1.2 move card & details popup render logic into the movie controller

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -1,10 +1,48 @@
 import FilmCardComponent from "../components/film-card.js";
 import DetailsPopupComponent from "../components/details-popup.js";
 
-import {remove, render, RenderPosition} from "../utils/render.js";
+import {render, RenderPosition} from "../utils/render.js";
 
 export default class MovieController {
-  constructor(container) {}
+  constructor(container) {
+    this._container = container;
+    this._cardComponent = null;
+    this._detailsPopupComponent = null;
 
-  render(card) {}
+    this._escKeyDownHandler = this._escKeyDownHandler.bind(this);
+  }
+
+  render(card) {
+    this._cardComponent = new FilmCardComponent(card);
+    this._detailsPopupComponent = new DetailsPopupComponent(card);
+
+    this._cardComponent.setClickHandler(() => {
+      this._openDetailsPopup();
+      document.addEventListener(`keydown`, this._escKeyDownHandler);
+    });
+
+    this._detailsPopupComponent.setCloseHandler(() => {
+      this._closeDetailsPopup();
+      document.removeEventListener(`keydown`, this._escKeyDownHandler);
+    });
+
+    render(this._container, this._cardComponent, RenderPosition.BEFOREEND);
+  }
+
+  _openDetailsPopup() {
+    render(document.body, this._detailsPopupComponent, RenderPosition.BEFOREEND);
+  }
+
+  _closeDetailsPopup() {
+    this._detailsPopupComponent.getElement().remove();
+  }
+
+  _escKeyDownHandler(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._closeDetailsPopup();
+      document.removeEventListener(`keydown`, this._escKeyDownHandler);
+    }
+  }
 }
--- src/controllers/page.js
+++ src/controllers/page.js
@@ -1,49 +1,14 @@
-import FilmCardComponent from "../components/film-card.js";
-import DetailsPopupComponent from "../components/details-popup.js";
 import NoCardsComponent from "../components/no-cards.js";
 import CardsComponent from "../components/cards.js";
 import LoadMoreBtnComponent from "../components/load-more-btn.js";
 import {SortType} from "../components/sort.js";
+import MovieController from "./movie.js";
 
 import {remove, render, RenderPosition} from "../utils/render.js";
 
 const SHOWING_CARD_COUNT_ON_START = 5;
 const SHOWING_CARD_COUNT_BY_BUTTON = 5;
 const SHOWING_EXTRA_CARD_COUNT = 2;
-
-
-const renderCard = (cardsListElement, card) => {
-  const openDetailsPopup = () => {
-    render(document.body, detailsPopupComponent, RenderPosition.BEFOREEND);
-  };
-
-  const closeDetailsPopup = () => {
-    detailsPopupComponent.getElement().remove();
-  };
-
-  const documentEscKeydownHandler = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      closeDetailsPopup();
-      document.removeEventListener(`keydown`, documentEscKeydownHandler);
-    }
-  };
-
-  const cardComponent = new FilmCardComponent(card);
-  cardComponent.setClickHandler(() => {
-    openDetailsPopup();
-    document.addEventListener(`keydown`, documentEscKeydownHandler);
-  });
-
-  const detailsPopupComponent = new DetailsPopupComponent(card);
-  detailsPopupComponent.setCloseHandler(() => {
-    closeDetailsPopup();
-    document.removeEventListener(`keydown`, documentEscKeydownHandler);
-  });
-
-  render(cardsListElement, cardComponent, RenderPosition.BEFOREEND);
-};
 
 const renderCards = (cardsListElement, cards) => {
   cards.forEach((card) => {

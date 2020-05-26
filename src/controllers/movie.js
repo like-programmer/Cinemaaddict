75a17cbe324c2ba414adbbb87e7e3a4e4a73b038
like-programmer <dashutkadevi@gmail.com>
2020-05-26 19:55:09 +0300

    6.1.1 create a card controller

---
+++ src/controllers/movie.js
@@ -0,0 +1,10 @@
+import FilmCardComponent from "../components/film-card.js";
+import DetailsPopupComponent from "../components/details-popup.js";
+
+import {remove, render, RenderPosition} from "../utils/render.js";
+
+export default class MovieController {
+  constructor(container) {}
+
+  render(card) {}
+}
